e0f99cc2d8f423f5c6effac093dade408fc5ee1c
akajprincess <jen18045@byui.com>
2019-02-16 20:18:53 -0700

    gfhjg'gy

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,2 +1,9 @@
-var d= new Date();
-document.getElementById ("currentdate").innerHTML=d
+let fulldate = new Date();
+let day = fulldate.getDate();
+let numericmonth = fulldate.getMonth();
+let year = fulldate.getFullYear();
+let numericweekday = fulldate.getDay();
+let month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+document.getElementById("currentdate").innerHTML = weekday[numericweekday] + ", "+day+ " "+month[numericmonth]+" "+year;
